f26e6c45c055df118f001b5c7ccf35b307e95f6f
Edwin Kofler <edwin@kofler.dev>
2021-05-20 01:59:42 -0700

    wip: rewrite parsing

--- lib/args-parse.sh
+++ lib/args-parse.sh
@@ -1,18 +1,9 @@
 # shellcheck shell=bash
 
 args.parse() {
-	# generate postArgs
-	local appendMode=no
-	for arg; do
-		if [ "$arg" = "--" ]; then
-			appendMode=yes
-		fi
-
-		if [ "$appendMode" = yes ]; then
-			postArgs+=("$arg")
-		fi
-	done
-
+	# Maps of flags and their values, required booleans,
+	# and their descriptions respectively
+	declare -A argsShortMap argsRequired argsDescription
 
 	local line
 	while IFS= read -r line; do
@@ -20,139 +11,156 @@ args.parse() {
 
 		local type="${line%% *}"
 		if [ "$type" = "@flag" ]; then
-			# Parse
+			# Parse lines; ensure each variable is blank if parsing did not find anything
+			# (corrects for parameter expansion behavior)
 			local flagNameOptional="${line##*[}"; flagNameOptional="${flagNameOptional%%]*}"
 			local flagNameRequired="${line##*<}"; flagNameRequired="${flagNameRequired%%>*}"
 			local flagValueDefault="${line##*\{}"; flagValueDefault="${flagValueDefault%%\}*}"
 			local flagDescription="${line##* -}"
-
-			# If no value was found, ensure the variable is empty. This accounts
-			# for the behavior of parameter expansion
 			if [ "$line" = "$flagNameOptional" ]; then flagNameOptional=; fi
 			if [ "$line" = "$flagNameRequired" ]; then flagNameRequired=; fi
 			if [ "$line" = "$flagValueDefault" ]; then flagValueDefault=; fi
 			if [ "$line" = "$flagDescription" ]; then flagDescription=; fi
 
-			# {
-			# 	echo ---
-			# 	echo "    flagNameOptional: $flagNameOptional"
-			# 	echo "    flagNameRequired: $flagNameRequired"
-			# 	echo "    flagValueDefault: $flagValueDefault"
-			# 	echo "    flagDescription: $flagDescription"
-			# 	echo ---
-			# } >&3
-
-			# Process the flag
+			# Sanity checks
 			if [ -z "$flagNameOptional" ] && [ -z "$flagNameRequired" ]; then
-				die 'args: Must specify either an optional or required flag'
+				echo "context: $line" >&2
+				die 'args: Must specify either an optional or required flag; neither were specified'
 			fi
 
 			if [ -n "$flagNameOptional" ] && [ -n "$flagNameRequired" ]; then
-				die 'args: Must specify either an optional or required flag; NOT both'
+				echo "context: $line" >&2
+				die 'args: Must specify either an optional or required flag; both were specified'
 			fi
 
-			# Set flagName, an agnostic reference of either optional or required
-			local flagName
-			if [ -n "$flagNameOptional" ]; then
-				flagName="$flagNameOptional"
-			else
-				flagName="$flagNameRequired"
-			fi
-
+			# Set flagName, which always has a value unlike
+			#flagNameOptional or flagNameRequired
+			local flagName="${flagNameOptional:-"$flagNameRequired"}"
 			local longFlag="${flagName%%.*}"
 			local shortFlag="${flagName##*.}"
-
-			# If shortFlag wasn't actually specified, ensure it is blank
 			if [ "$flagName" = "$shortFlag" ]; then shortFlag=; fi
 
-			# Set the currentFlag, mainly for printing
-			local currentFlag
+			# Set argsRequired
+			if [ -z "$flagNameRequired" ]; then
+				if [ -n "$longFlag" ]; then
+					argsRequired+=(["$longFlag"]="yes")
+				fi
+
+				if [ -n "$shortFlag" ]; then
+					argsRequired+=(["$shortFlag"]="yes")
+				fi
+			fi
+
+			# Set argsDescription
 			if [ -n "$longFlag" ]; then
-				currentFlag="--$longFlag"
-			else
-				currentFlag="-$shortFlag"
+				argsDescription+=(["$longFlag"]="$flagDescription")
 			fi
 
-			# {
-			# 	echo "    longFlag: $longFlag"
-			# 	echo "    shortFlag: $shortFlag"
-			# 	echo ---
-			# } >&3
+			if [ -n "$shortFlag" ]; then
+				argsDescription+=(["$shortFlag"]="$flagDescription")
+			fi
 
-			# Look for the matching argument, then stay in loop
-			# one extra iteration to get the value of the argument
-			local arg flagWasFound=no didImmediateBreak=no
-			for arg; do
-				if [ "$flagWasFound" = yes ]; then
-					didImmediateBreak=yes
-					break
-				fi
-
-				if [ "$arg" = "--$longFlag" ]; then
-					flagWasFound=yes
-				elif [ "$arg" = "-$shortFlag" ]; then
-					flagWasFound=yes
-				fi
-			done
-			local flagValueCli="${arg:-}"
-
-			# If the flag name is required, we exit a failure if it's not there
-			if [ -n "$flagNameRequired" ]; then
-				# If we did not set flagWasFound=yes, it means it did not find
-				# the flag. So, if the flag is <required>, we fail right away
-				if [ "$flagWasFound" = no ]; then
-					die "args: You must supply the flag '$currentFlag' with a value"
-				fi
-
-				# If we were supposed to do an immediate break, but didn't actually
-				# do it, it means we are on the last argument and there is no value
-				if [ "$flagWasFound" = yes ] && [ "$didImmediateBreak" = no ]; then
-					die "args: No value found for flag '$currentFlag'"
+			# Set argsShortMap
+			if [ -n "$shortFlag" ]; then
+				if [ -n "$longFlag" ]; then
+					argsShortMap+=(["-$shortFlag"]="$longFlag")
 				fi
 			fi
 
-			# Set the default for the current flag. If there is no default,
-			# it is just an empty assignment
+			# Set args
+			# Only set shortFlag if longFlag isn't defined
 			if [ -n "$longFlag" ]; then
 				args+=(["$longFlag"]="$flagValueDefault")
 			elif [ -n "$shortFlag" ]; then
 				args+=(["$shortFlag"]="$flagValueDefault")
-			fi
-
-
-			# There is a flag with a possible value
-			# the didImmediateBreak check ensures that the value of "$arg" isn't
-			# the same as the last element (which is the flag option itself)
-			if [ "$flagWasFound" = yes ] && [ "$didImmediateBreak" = yes ]; then
-				case "$flagValueCli" in
-					-*)
-						die "args: You must supply a value for '$currentFlag'"
-						;;
-					*)
-						# The user-supplied flag is valid, override the default
-						if [ -n "$longFlag" ]; then
-							args+=(["$longFlag"]="$arg")
-						elif [ -n "$shortFlag" ]; then
-							args+=(["$shortFlag"]="$arg")
-						fi
-				esac
+			else
+				echo "context: $line" >&2
+				die "args: Neither a short flag or a long flag were specified"
 			fi
 		elif [ "$type" = "@arg" ]; then
 			:
 		else
+			echo "context: $line" >&2
 			die "args: Pragma must be either @flag or @arg"
 		fi
 	done
 
-	# {
-	# 	echo +++
-	# 	for i in "${!args[@]}"; do
-	# 		echo "key  : $i"
-	# 		echo "value: ${args[$i]}"
-	# 		echo
-	# 	done
-	# 	echo +++
-	# 	echo; echo; echo
-	# } >&3
+	# Parse the arguments, either 'read'ing or 'set'ing the values
+	local -a nonFlagArgs
+	# shellcheck disable=SC1007
+	local arg= previousArg= forAction=read
+	# shellcheck disable=SC2209,SC2192
+	for arg; do
+		echo "LOOP: $arg: $previousArg: $forAction" >&3
+		case "$forAction" in
+		read)
+			case "$arg" in
+				-*)
+					previousArg="$arg"
+					forAction=write
+					continue
+					;;
+				*)
+					nonFlagArgs+=("$arg")
+					continue
+					;;
+			esac
+			;;
+		write)
+			case "$arg" in
+				# The previous flag was specified, but doesn't have
+				# a value. Save the previous flag, and read the current
+			-*)
+				if [ -n "$flagNameRequired" ]; then
+					die "Flag '$previousArg' is required as it's marked as required"
+				else
+					args+=(["$previousArg"]=)
+					previousArg="$arg"
+					continue
+				fi
+				;;
+
+				# Read in new value
+			*)
+				# If previous value is long
+				# previousArg="${previousArg/--/-}"
+				# previousArg="${previousArg#-}"
+
+				case "$previousArg" in
+				--*)
+					args+=(["$previousArg"]="$arg")
+					;;
+				-*)
+					local currentLongValue="${argsShortMap["$arg"]}"
+					args+=(["$currentLongValue"]="$arg")
+					;;
+				*)
+					# TODO
+					die 'This is not supposed to happen'
+					;;
+				esac
+
+				forAction=read
+				continue
+			esac
+			;;
+		esac
+	done
+
+	echo --- >&3
+	local key value
+	for key in "${!args[@]}"; do
+		value="${args["$key"]}"
+
+		echo "$key: $value" >&3
+	done
+
+	if [ "$forAction" = 'write' ]; then
+		# and if 'required'
+		# die "args: No value found for flag '$arg:-'"
+		:
+	fi
+
+
 
 }
